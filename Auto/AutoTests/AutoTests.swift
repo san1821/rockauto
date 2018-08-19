930e491df99ba59aba021acf92a427cf6426e51d
Sanjay Chaudhari <sanjaychaudhari@San.local>
2018-08-19 23:26:42 +0530

    try

---
+++ Auto/AutoTests/AutoTests.swift
@@ -0,0 +1,36 @@
+//
+//  AutoTests.swift
+//  AutoTests
+//
+//  Created by Sanjay Chaudhari on 19/08/18.
+//  Copyright © 2018 Sanjay Chaudhari. All rights reserved.
+//
+
+import XCTest
+@testable import Auto
+
+class AutoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
